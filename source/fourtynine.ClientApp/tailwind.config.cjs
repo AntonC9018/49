a7fb21db37de2dfe7db92e5d16b73eea64d7eea3
AntonC9018 <anton.curm@gmail.com>
2022-11-10 23:33:53 +0200

    check if tailwind actually works

--- source/fourtynine.ClientApp/tailwind.config.cjs
+++ source/fourtynine.ClientApp/tailwind.config.cjs
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "../fourtynine/Views/**/*.cshtml",
     "./src/**/*.{js,ts}",
   ],
   theme: {
+    colors,
     extend: {},
   },
   plugins: [],
--- source/fourtynine.ClientApp/vite.config.ts
+++ source/fourtynine.ClientApp/vite.config.ts
@@ -1,15 +1,8 @@
 ﻿import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "url";
-import * as path from "path";
-
-let aspnetProxy = {
-    target: "https://localhost:7186",
-    secure: false
-};
 
 const vitePort = 5173;
 
-// https://vitejs.dev/config/
 export default defineConfig({
     build: {
         manifest: true,
@@ -32,7 +25,7 @@ export default defineConfig({
         hmr: {
             protocol: "wss",
             
-            // Here, we have to specify something that doesn"t get mapped to an ASP.NET Core controller.
+            // Here, we have to specify something that doesn't get mapped to an ASP.NET Core controller.
             // Otherwise, all websocket connection attempts get processed by APS.NET Core, without forwarding
             // them to the vite server, which makes the client fail to connect to the vite server.
             path: "vite-ws",
